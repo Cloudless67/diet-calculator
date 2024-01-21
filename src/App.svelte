91a210c2ace5b681b8a45887216701a90124ec85
장태훈 <tajang@gmarket.com>
2024-01-21 18:41:40 +0900

    활동계수, 성별 추가

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,39 @@
   import { onMount } from 'svelte';
   import NumberInput from './lib/NumberInput.svelte';
   import formatNumber from './utils/formatNumber';
+  import SelectInput from './lib/SelectInput.svelte';
 
+  let sex: number | null = null;
   let intensity: number | null = null;
   let fat: number | null = null;
   let weight: number | null = null;
   let height: number | null = null;
   let age: number | null = null;
   let activity: number | null = null;
+  let exercise: number | null = null;
 
   onMount(() => {
+    sex = Number(localStorage.getItem('sex') ?? '0');
     intensity = Number(localStorage.getItem('intensity') ?? '0');
     fat = Number(localStorage.getItem('fat') ?? '0');
     weight = Number(localStorage.getItem('weight') ?? '0');
     height = Number(localStorage.getItem('height') ?? '0');
     age = Number(localStorage.getItem('age') ?? '0');
     activity = Number(localStorage.getItem('activity') ?? '0');
+    exercise = Number(localStorage.getItem('exercise') ?? '0');
   });
 
   $: proteinPerWeight = fat && 4.8 - fat / 10;
   $: basalMetabolicRate =
-    weight && height && age && 88.4 + 13.4 * weight + 4.8 * height - 5.68 * age;
+    weight &&
+    height &&
+    age &&
+    (sex === 1
+      ? 88.4 + 13.4 * weight + 4.8 * height - 5.68 * age
+      : 447.6 + 9.25 * weight + 3.1 * height - 4.33 * age);
+  $: activityCoeff = activity && exercise !== null ? activity + exercise : 0;
   $: dailyCalorieConsumption =
-    basalMetabolicRate && activity && basalMetabolicRate * activity;
+    basalMetabolicRate && activityCoeff && basalMetabolicRate * activityCoeff;
   $: caloriesToEat =
     dailyCalorieConsumption &&
     intensity &&
@@ -47,6 +58,17 @@
 <main class="container pt-3">
   <form class="row">
     <div class="col-lg-4">
+      <SelectInput
+        id="sex"
+        label="성별"
+        options={[
+          { value: 1, label: '남성' },
+          { value: 2, label: '여성' },
+        ]}
+        bind:value={sex}
+      />
+    </div>
+    <div class="col-lg-4">
       <NumberInput
         id="intensity"
         label="체중감량 강도 1~5 (4,5는 근손실 가능성 존재)"
@@ -68,7 +90,29 @@
       <NumberInput id="age" label="나이(만)" bind:value={age} />
     </div>
     <div class="col-lg-4">
-      <NumberInput id="activity" label="활동계수" bind:value={activity} />
+      <SelectInput
+        id="activity"
+        label="활동량"
+        options={[
+          { value: 1.2, label: '적은 활동' },
+          { value: 1.4, label: '중간 활동' },
+          { value: 1.6, label: '많은 활동' },
+        ]}
+        bind:value={activity}
+      />
+    </div>
+    <div class="col-lg-4">
+      <SelectInput
+        id="exercise"
+        label="운동량"
+        options={[
+          { value: 0, label: '주 0회' },
+          { value: 0.1, label: '주 1~3회' },
+          { value: 0.2, label: '주 4~6회' },
+          { value: 0.3, label: '주 7회 이상' },
+        ]}
+        bind:value={exercise}
+      />
     </div>
   </form>
   <table class="table table-striped">
@@ -78,7 +122,12 @@
         <td>{formatNumber(proteinPerWeight)}</td>
       </tr>
       <tr>
-        <td>기초대사량(kcal)</td>
+        <td
+          ><details>
+            해당 기초대사량은 Haris-Benedict 방정식을 사용하여 계산되었습니다
+            <summary>기초대사량(kcal)</summary>
+          </details></td
+        >
         <td>{formatNumber(basalMetabolicRate)}</td>
       </tr>
       <tr>
@@ -120,3 +169,15 @@
     </tbody>
   </table>
 </main>
+
+<style>
+  details > summary {
+    list-style: none;
+  }
+  summary::after {
+    content: ' ►';
+  }
+  details[open] summary:after {
+    content: ' ▼';
+  }
+</style>
---
+++ src/lib/SelectInput.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let id: string;
+  export let value: number | null = null;
+  export let options: { value: number; label: string }[] = [];
+  export let label = '';
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+
+    localStorage.setItem(id, target.value);
+  }
+</script>
+
+<div class="mb-3">
+  <label for={id} class="form-label">{label}</label>
+  <select {id} class="form-select" bind:value on:change={handleChange}>
+    {#each options as options}
+      <option value={options.value}>{options.label}</option>
+    {/each}
+  </select>
+</div>
